181548866b15a99d0c5a0e33693000e281ff0d00
Angelica Larsson <angelicacatherinelarsson@gmail.com>
2026-06-03 20:05:58 +0200

    Implement cd command

---
+++ app/commands.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"slices"
+	"strings"
+)
+
+type Command interface {
+	Execute(args []string) error
+}
+
+type ExitCommand struct{}
+
+func (c *ExitCommand) Execute(args []string) error {
+	os.Exit(0)
+
+	return nil
+}
+
+type EchoCommand struct{}
+
+func (c *EchoCommand) Execute(args []string) error {
+	msg := strings.Join(args, " ")
+	fmt.Println(msg)
+
+	return nil
+}
+
+type TypeCommand struct {
+	builtins []string
+}
+
+func (c *TypeCommand) Execute(args []string) error {
+	for _, arg := range args {
+
+		if slices.Contains(c.builtins, arg) {
+			fmt.Printf("%s is a shell builtin\n", arg)
+			continue
+		}
+
+		path, err := exec.LookPath(arg)
+		if err != nil {
+			fmt.Printf("%s not found\n", arg)
+			continue
+		}
+
+		fmt.Printf("%s is %s\n", arg, path)
+	}
+
+	return nil
+}
+
+type ExternalCommand struct{}
+
+func (c *ExternalCommand) Execute(args []string) error {
+	targetDir := args[0]
+
+	_, err := exec.LookPath(args[0])
+
+	if err != nil {
+		fmt.Printf("%s: command not found\n", targetDir)
+		return nil
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
+	return nil
+}
+
+type PwdCommand struct{}
+
+func (c *PwdCommand) Execute(args []string) error {
+	path, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	fmt.Println(path)
+
+	return nil
+}
+
+type CdCommand struct{}
+
+func (c *CdCommand) Execute(args []string) error {
+	targetDir := args[0]
+
+	err := os.Chdir(targetDir)
+	if err != nil {
+		fmt.Printf("cd: %s: No such file or directory\n", targetDir)
+	}
+
+	return nil
+}
--- app/main.go
+++ app/main.go
@@ -4,104 +4,48 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"os/exec"
-	"slices"
 	"strings"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
+	commands := make(map[string]Command)
+
+	commands["exit"] = &ExitCommand{}
+	commands["echo"] = &EchoCommand{}
+
+	builtinNames := []string{"echo", "exit", "type", "pwd", "cd"}
+	commands["type"] = &TypeCommand{
+		builtins: builtinNames,
+	}
+	commands["pwd"] = &PwdCommand{}
+	commands["cd"] = &CdCommand{}
+
 	for {
 		fmt.Print("$ ")
 
-		line, err := reader.ReadString('\n')
-
+		input, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			os.Exit(1)
-		}
-		cmdLine := strings.TrimSpace(line)
-
-		words := strings.Split(cmdLine, " ")
-
-		//handleExecutable(cmd, args)
-		cmd := words[0]
-		args := words[1:]
-
-		switch cmd {
-
-		case "exit":
-			handleExit()
-		case "echo":
-			handleEcho(args)
-		case "type":
-			handleType(args)
-		case "pwd":
-			handlePrintWorkingDirectory()
-		default:
-			if handleExecutable(words) != nil {
-				fmt.Printf("%s: command not found\n", cmd)
-
-			}
+			break
 		}
 
-	}
-}
-
-func handleExit() {
-	os.Exit(0)
-}
-
-func handleEcho(args []string) {
-	msg := strings.Join(args, " ")
-
-	fmt.Println(msg)
-}
-
-func handleType(args []string) {
-	builtins := []string{"exit", "echo", "type", "pwd"}
-
-	for _, arg := range args {
-
-		if slices.Contains(builtins, arg) {
-			fmt.Printf("%s is a shell builtin\n", arg)
+		input = strings.TrimSpace(input)
+		if input == "" {
 			continue
 		}
 
-		path, err := exec.LookPath(arg)
-		if err != nil {
-			fmt.Printf("%s not found\n", arg)
-			continue
+		parts := strings.Fields(input)
+		cmdName := parts[0]
+		args := parts[1:]
+
+		if command, exists := commands[cmdName]; exists {
+			command.Execute(args)
+		} else {
+			externalCmd := &ExternalCommand{}
+			externalCmd.Execute(parts)
 		}
 
-		fmt.Printf("%s is %s\n", arg, path)
-
-	}
-}
-
-func handleExecutable(args []string) error {
-
-	_, err := exec.LookPath(args[0])
-
-	if err != nil {
-		return err
 	}
 
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
-
-	return nil
-}
-
-func handlePrintWorkingDirectory() {
-	path, err := os.Getwd()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(path)
 }
